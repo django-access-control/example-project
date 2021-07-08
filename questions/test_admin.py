31e1f39148a88a6c68767aa427680e4ab7ef7cae
eeriksp <eerik.sven@gmail.com>
2021-07-08 23:12:35 +0300

    Add integration tests for add view

--- questions/test_admin.py
+++ questions/test_admin.py
@@ -37,7 +37,7 @@ class ListViewTest(BaseAdminTestCase):
         self.assertTrue("Imsum" in table)
 
     def test_anonymous_user_sees_only_published_questions(self):
-        table = self.get_list_page_table(self.user_1_client)
+        table = self.get_list_page_table(self.anonymous_client)
         self.assertTrue("Lorem" in table)
         self.assertTrue("Imsum" not in table)
 
@@ -50,3 +50,27 @@ class ListViewTest(BaseAdminTestCase):
         table = self.get_list_page_table(self.user_2_client)
         self.assertTrue("Lorem" in table)  # This one is published
         self.assertTrue("Imsum" in table)  # This one is not published, but user_2 sees it because they are the author
+
+
+class AddViewTest(BaseAdminTestCase):
+
+    @staticmethod
+    def get_add_page_form(client: Client):
+        response = client.get("/questions/question/add", follow=True)
+        return str(BeautifulSoup(response.content, 'html.parser').find(id="question_form"))
+
+    def test_staff_member_can_submit_three_fields(self):
+        table = self.get_add_page_form(self.staff_member_client)
+        self.assertTrue('<div class="form-row field-title">' in table)
+        self.assertTrue('<div class="form-row field-body">' in table)
+        self.assertTrue('<div class="form-row field-is_published">' in table)
+
+    def test_anonymous_user_cannot_add_question(self):
+        response = self.anonymous_client.get("/questions/question/add", follow=True)
+        self.assertEqual(response.status_code, 403)
+
+    def test_regular_user_can_submit_two_fields(self):
+        table = self.get_add_page_form(self.user_1_client)
+        self.assertTrue('<div class="form-row field-title">' in table)
+        self.assertTrue('<div class="form-row field-body">' in table)
+        self.assertTrue('<div class="form-row field-is_published">' not in table)
